814e8799b8b8436d366b4d0abab1371c32ee3fb7
SiewYan <siew.yan.hoh@cern.ch>
2018-01-23 09:02:43 -0600

    Re-base the combined datacard creation directory

--- datacards/monox_datacards/run_combineMassBin.sh
+++ datacards/monox_datacards/run_combineMassBin.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-
+dir=`pwd`
 for massbin in mass0 mass1 mass2 mass3
 do
+eval cd $dir
+scp massbin_combine.py ./$massbin
+eval cd ./$massbin
 comx=`(python massbin_combine.py $massbin)`
-echo $comx
+echo $comx > run.sh
+chmod 775 run.sh
+rm massbin_combine.py
+sh run.sh
 done
 echo "Copy the output and execute in terminal, sorry, IO is not working"
